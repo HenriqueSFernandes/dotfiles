c2e54c01ecd24c69eb5c4de40b2a02cd72df92ca
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-08-01 17:01:35 +0100

    Updated lunarvim config

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,15 +1,6 @@
--- Read the docs: https://www.lunarvim.org/docs/configuration
--- Video Tutorials: https://www.youtube.com/watch?v=sFA9kX-Ud_c&list=PLhoH5vyxr6QqGu0i7tt_XoVK9v-KvZ3m6
--- Forum: https://www.reddit.com/r/lunarvim/
--- Discord: https://discord.com/invite/Xb9B4Ny
--- install plugins
-lvim.plugins = {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-  { "mhartington/formatter.nvim", name = "formatter", priority = 900},
-}
-
-vim.cmd("set hlsearch")
-vim.cmd("set relativenumber")
+reload("user.options")
+reload("user.plugins")
+reload("user.keybinds")
 
 local formatters = require "lvim.lsp.null-ls.formatters"
 formatters.setup {
@@ -25,4 +16,4 @@ linters.setup {
   },
 }
 
-lvim.transparent_window = true
+
---
+++ .config/lvim/lua/user/options.lua
@@ -0,0 +1,6 @@
+vim.opt.relativenumber = true
+vim.opt.hlsearch = true
+
+lvim.colorscheme = "catppuccin-mocha"
+lvim.transparent_window = false
+
