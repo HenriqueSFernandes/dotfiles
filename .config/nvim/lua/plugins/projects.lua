3a37fd6d6e25f19a0282d9ced8145da7188a1310
HenriqueSFernandes <henriquesardofernandes@gmail.com>
2024-08-16 16:14:36 +0100

    Added project manager to nvim

---
+++ .config/nvim/lua/plugins/projects.lua
@@ -0,0 +1,27 @@
+return {
+	"coffebar/neovim-project",
+	opts = {
+		projects = { -- define project roots
+			"~/.config/kitty",
+			"~/.config/nvim",
+			"~/.config/tmux",
+			"~/.config/hypr",
+			"~/Desktop/Repositories/*",
+			"~/Desktop/nodeLearn",
+			"~/Desktop/leetcode",
+			"~/Desktop/GoX-App",
+		},
+	},
+	init = function()
+		-- enable saving the state of plugins in the session
+		vim.opt.sessionoptions:append("globals") -- save global variables that start with an uppercase letter and contain at least one lowercase letter.
+		vim.keymap.set({ "n", "v" }, "<C-l>", "<cmd>:Telescope neovim-project discover<cr>", { desc = "Projects" })
+	end,
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{ "nvim-telescope/telescope.nvim", tag = "0.1.4" },
+		{ "Shatur/neovim-session-manager" },
+	},
+	lazy = false,
+	priority = 100,
+}
